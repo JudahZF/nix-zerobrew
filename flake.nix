1caf2a3cfe43eb87ef6f4dc0cab0818242db139c
Steve Mayer <smayer69@me.com>
2026-03-20 21:44:36 -0600

    Updated zerobrew to 0.2.1

--- flake.nix
+++ flake.nix
@@ -12,70 +12,99 @@
       type = "github";
       owner = "lucasgelfond";
       repo = "zerobrew";
-      ref = "v0.1.2";
+      ref = "v0.2.1";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, zerobrew-src }: let
-    # Systems supported by zerobrew (macOS only)
-    systems = [ "aarch64-darwin" "x86_64-darwin" ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      zerobrew-src,
+    }:
+    let
+      # Systems supported by zerobrew (macOS only)
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-    pkgsFor = system: import nixpkgs {
-      inherit system;
-      overlays = [ rust-overlay.overlays.default ];
-    };
-
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
-      f system (pkgsFor system)
-    );
-  in {
-    packages = forAllSystems (system: pkgs: {
-      zerobrew = let
-        zerobrewRust = pkgs.rust-bin.stable."1.90.0".default;
-      in pkgs.callPackage ./pkgs/zerobrew {
-        inherit zerobrew-src;
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = zerobrewRust;
-          rustc = zerobrewRust;
+      pkgsFor =
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
         };
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system (pkgsFor system));
+    in
+    {
+      packages = forAllSystems (
+        system: pkgs: {
+          zerobrew =
+            let
+              zerobrewRust = pkgs.rust-bin.stable."1.90.0".default;
+            in
+            pkgs.callPackage ./pkgs/zerobrew {
+              inherit zerobrew-src;
+              rustPlatform = pkgs.makeRustPlatform {
+                cargo = zerobrewRust;
+                rustc = zerobrewRust;
+              };
+            };
+
+          default = self.packages.${system}.zerobrew;
+        }
+      );
+
+      checks = forAllSystems (
+        system: pkgs: {
+          zerobrew-help =
+            pkgs.runCommandLocal "zerobrew-help"
+              {
+                nativeBuildInputs = [ self.packages.${system}.zerobrew ];
+              }
+              ''
+                zb --help > "$out"
+              '';
+        }
+      );
+
+      darwinModules = rec {
+        nix-zerobrew =
+          { lib, pkgs, ... }:
+          {
+            imports = [
+              ./modules
+            ];
+            nix-zerobrew.package =
+              lib.mkOptionDefault
+                self.packages.${pkgs.stdenv.hostPlatform.system}.zerobrew;
+            nix-zerobrew.packageRosetta = lib.mkOptionDefault self.packages.x86_64-darwin.zerobrew;
+          };
+
+        default = nix-zerobrew;
       };
 
-      default = self.packages.${system}.zerobrew;
-    });
-
-    checks = forAllSystems (system: pkgs: {
-      zerobrew-help = pkgs.runCommandLocal "zerobrew-help" {
-        nativeBuildInputs = [ self.packages.${system}.zerobrew ];
-      } ''
-        zb --help > "$out"
-      '';
-    });
-
-    darwinModules = rec {
-      nix-zerobrew = { lib, pkgs, ... }: {
-        imports = [
-          ./modules
-        ];
-        nix-zerobrew.package = lib.mkOptionDefault self.packages.${pkgs.stdenv.hostPlatform.system}.zerobrew;
-        nix-zerobrew.packageRosetta = lib.mkOptionDefault self.packages.x86_64-darwin.zerobrew;
-      };
-
-      default = nix-zerobrew;
+      devShells = forAllSystems (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            buildInputs =
+              with pkgs;
+              [
+                rustc
+                cargo
+                openssl
+                pkg-config
+              ]
+              ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+                pkgs.apple-sdk_15
+                (pkgs.darwinMinVersionHook "10.15")
+              ];
+          };
+        }
+      );
     };
-
-    devShells = forAllSystems (system: pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustc
-          cargo
-          openssl
-          pkg-config
-        ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-          pkgs.apple-sdk_15
-          (pkgs.darwinMinVersionHook "10.15")
-        ];
-      };
-    });
-  };
 }
